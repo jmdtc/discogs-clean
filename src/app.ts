003c87bb9bf9d313c865d50a5450d18f61de389e
Mathieu Delisle <mathieudelisle@outlook.com>
2022-01-02 16:09:50 +0100

    WIP

--- src/app.ts
+++ src/app.ts
@@ -10,9 +10,6 @@ import searchForSimilarTracks, {
 
   const app = express();
   const port = 3000;
-  app.get("/", (req, res) => {
-    res.send("The sedulous hyena ate the antelope!");
-  });
 
   app.listen(port, () => {
     console.log(`server is listening on port: ${port}`);
